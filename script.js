4f637702045db10f57d2fea050dcf991f1265ba5
Daniel-Olofsson <daniel-olofsson@hotmail.se>
2022-09-26 18:15:04 +0200

    SVG shopping cart, modal, search bar

--- script.js
+++ script.js
@@ -40,4 +40,29 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-} 
+}
+// modal for cart
+var modal = document.getElementById("myModalcart");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myOnclick");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
