5fd91c404cb81d46e794727ed315a75ba98afd7d
Emmvid <emmavidarsson@hotmail.com>
2022-09-26 15:15:35 +0200

    added back to top + changes in contact

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ try {
   adbnrefresh.addEventListener("click", function () {
     location.reload();
   });
+  
+// back to top button //
+  let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+} 
